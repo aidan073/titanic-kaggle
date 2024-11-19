722e105ac0b2f96d90647c8cfb07f89295baf011
aidan073 <aidanbell073@gmail.com>
2024-11-19 18:41:11 -0500

    finalized submission

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import preprocess
-import numpy as np
+import pandas as pd
+import argparse
 from sklearn.svm import SVC
 
-train_path = "data/train.csv"
-test_path = "data/test.csv" 
+# arg parsing for command line
+parser = argparse.ArgumentParser(description="Titanic Kaggle")
+parser.add_argument('train_path', type=str, help="Path to the training dataset")
+parser.add_argument('test_path', type=str, help="Path to the test dataset")
+args = parser.parse_args()
 
 # preprocessing
-train_df = preprocess.get_df(train_path)
-test_df = preprocess.get_df(test_path)
-labels = preprocess.seperate_labels(train_df)
+train_df = preprocess.get_df(args.train_path)
+test_df = preprocess.get_df(args.test_path)
+labels_train, ids_test = preprocess.seperate_labels(train_df, test_df)
 train_df = preprocess.feature_engineering(train_df)
-print(train_df.head())
-print(train_df.isnull().sum())
+test_df = preprocess.feature_engineering(test_df)
+test_df = test_df.reindex(columns=train_df.columns, fill_value=0) # ensure test has same columns as train (necessary due to get_dummies)
 
-# X = 
-# y = data.target 
+# SVM
+svm_model = SVC(kernel='rbf', C=1.0, random_state=73)
+svm_model.fit(train_df, labels_train)
+y_pred = svm_model.predict(test_df)
 
-# svm_model = SVC(kernel='rbf', C=1.0, random_state=73)
-# svm_model.fit(X_train, y_train)
-# y_pred = svm_model.predict(X_test)
+# create submission file
+submission = pd.DataFrame({
+    'PassengerId': ids_test,
+    'Survived': y_pred
+})
+submission.to_csv('submission.csv', index=False)
+print("Complete")
--- preprocess.py
+++ preprocess.py
@@ -1,44 +1,59 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # get dataframe from csv
 def get_df(data_path:str):
     return pd.read_csv(data_path)
 
 # seperate labels from training df
-def seperate_labels(df:pd.DataFrame):
-    labels = list(df["Survived"])
-    df.drop("Survived", axis=1, inplace=True)
-    return labels
+def seperate_labels(train_df:pd.DataFrame, test_df:pd.DataFrame):
+    labels = list(train_df["Survived"])
+    train_df.drop("Survived", axis=1, inplace=True)
+    train_df.drop("PassengerId", axis=1, inplace=True)
+
+    ids = list(test_df["PassengerId"])
+    test_df.drop("PassengerId", axis=1, inplace=True)
+    return labels, ids
 
 def get_prefix(ticket):
-    lead = ticket.split(' ')[0][0]
+    lead = ticket.split(" ")[0][0]
     if lead.isalpha():
-        return ticket.split(' ')[0]
+        return ticket.split(" ")[0]
     else:
-        return 'NoPrefix'
+        return "NoPrefix"
 
 def feature_engineering(df:pd.DataFrame):
     # get lastnames and family size
-    df = df.assign(lastName=df["Name"].apply(lambda x: x.split(",")[0].strip().lower()))
-    df.drop("Name", axis=1, inplace=True)
-    df["familySize"] = df.groupby("lastName")["lastName"].transform("count")
-    df["Age"] = df["Age"].fillna(df["Age"].mean())
-    df["Cabin"] = df["Cabin"].fillna(df["Cabin"].mode()[0])
+    df = df.assign(Title=df["Name"].apply(lambda x: x.split(" ")[1].strip().lower())) # get Title i.e. Mr Mrs
+    df["HasCabin"] = df["Cabin"].notnull().astype(int) # bool for if sample has a cabin
+    df["Deck"] = df["Cabin"].str[0] # get deck
     df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
+    df["Sex"] = df["Sex"].map({"male": 1, "female": 0})
+
+    # get dummies for categoricals
+    df = pd.get_dummies(df, columns=["Embarked"], prefix="Embarked", drop_first=False)
+    df = pd.get_dummies(df, columns=["Deck"], prefix="Deck", drop_first=False)
+    df = pd.get_dummies(df, columns=["Title"], prefix="Title", drop_first=False)
+
+    # normalize
+    scaler = MinMaxScaler()
+    df["Fare"] = scaler.fit_transform(df["Fare"].fillna(df["Fare"].median()).values.reshape(-1, 1))
+    df["FamilySize"] = scaler.fit_transform((df["SibSp"] + df["Parch"] + 1).values.reshape(-1,1))
+    df["Age"] = scaler.fit_transform(df["Age"].fillna(df["Age"].mean()).values.reshape(-1,1))
+
+    # remove un-needed rows
+    df = df.drop(columns=["Name","Cabin","SibSp", "Parch"])
 
     # process tickets, referenced kaggle user "Oscar Takeshita"
-    # df['Ticket'] = df['Ticket'].replace('LINE','LINE 0')
-    # df['Ticket'] = df['Ticket'].apply(lambda x: x.replace('.','').replace('/','').lower())
-    # df['Prefix'] = df['Ticket'].apply(lambda x: get_prefix(x))
-    # df['TNumeric'] = df['Ticket'].apply(lambda x: int(x.split(' ')[-1])//1)
-    # df['TNlen'] = df['TNumeric'].apply(lambda x : len(str(x)))
-    # df['LeadingDigit'] = df['TNumeric'].apply(lambda x : int(str(x)[0]))
-    # df['TGroup'] = df['Ticket'].apply(lambda x: str(int(x.split(' ')[-1])//10))
-    # df = df.drop(columns=['Ticket','TNumeric','Pclass'])
-    # df = df.join(pd.get_dummies(df[['Prefix', 'TGroup']], dtype=int))
-    # df.head()  
+    df["Ticket"] = df["Ticket"].replace("LINE","LINE 0")
+    df["Ticket"] = df["Ticket"].apply(lambda x: x.replace(".","").replace("/","").lower())
+    df["Prefix"] = df["Ticket"].apply(lambda x: get_prefix(x))
+    df["TNumeric"] = df["Ticket"].apply(lambda x: int(x.split(" ")[-1])//1)
+    df["TNlen"] = df["TNumeric"].apply(lambda x : len(str(x)))
+    df["LeadingDigit"] = df["TNumeric"].apply(lambda x : int(str(x)[0]))
+    df["TGroup"] = df["Ticket"].apply(lambda x: str(int(x.split(" ")[-1])//10))
+    df = df.join(pd.get_dummies(df[["Prefix", "TGroup"]], dtype=int))
+    scaler.fit_transform(df["TNlen"].values.reshape(-1,1))
+    scaler.fit_transform(df["LeadingDigit"].values.reshape(-1,1))
+    df = df.drop(columns=["Ticket","TNumeric","Pclass", "Prefix", "TGroup"])
     return df
-
-# normalize features
-def normalize():
-    pass
